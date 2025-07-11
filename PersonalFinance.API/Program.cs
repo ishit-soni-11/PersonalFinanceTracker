7a0350b51a5109dc08f1722d1afc1668de73ae9e
ishit-soni-11 <ishitsoni1418@gmail.com>
2025-07-11 15:25:38 -0400

    Install Chart.js for expense visualization

--- PersonalFinance.API/Program.cs
+++ PersonalFinance.API/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         builder =>
         {
-            builder.WithOrigins("http://localhost:3000")
+            builder.WithOrigins("http://localhost:3000", "http://localhost:3001")  // Added 3001
                    .AllowAnyHeader()
                    .AllowAnyMethod();
         });
